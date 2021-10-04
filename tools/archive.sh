48dcc4cff4ddaccfea8e671f5425a94bfc85134f
Diana Caisin <diana.caisin@coxautoinc.com>
2021-10-04 19:12:03 -0400

    feature/dc-cplat-271: updated archive script

--- tools/archive.sh
+++ tools/archive.sh
@@ -1,28 +1,31 @@
 #!/usr/bin/env bash
-#Create tar.gz archive with all .py, .html and .htm files within the repo and list all files for fun
-if [[ -f ".circleci/debuglog" ]]; then
-	set -x
-fi
 
+#Create tar.gz archive with all .py, .html and .htm files within the repo and list all files for fun
+#if [[ -f ".circleci/debuglog" ]]; then
+#	set -x
+#fi
 WORKINGDIR="${CIRCLE_WORKING_DIRECTORY}"
 REPONAME="${CIRCLE_PROJECT_REPONAME}"
-TEMP_DIR="/tmp/archives"
-pyfiles=$(find . \( -name "*.py" -o -name "*.html" -o -name "*.htm" -o -name "Pipfile.lock" \))
+
+mkdir -p /tmp/archives
+export TEMP_DIR="/tmp/archives"
 
 cd "$WORKINGDIR" || echo "no circle_working_directory defined"
-shopt -s nullglob
+#shopt -s nullglob
+FILES=$(find . \( -name "*.py" -o -name "*.html" -o -name "*.htm" -o -name "Pipfile.lock" \))
+print "$FILES" > "$REPONAME.txt"
 
-print "${pyfiles}" > "${REPONAME}.txt"
-
-tar czf "${REPONAME}.tar.gz" -T "${REPONAME}.txt" && rm -f "${REPONAME}.txt"
-
-mkdir -p "${TEMP_DIR}" && mv "${REPONAME}.tar.gz" "${TEMP_DIR}"
-
-export ARTIFACT_ZIP_PATH="${TEMP_DIR}/${REPONAME}.tar.gz"
-
+tar czf "$REPONAME.tar.gz" -T "$REPONAME.txt"
+rm -f "$REPONAME.txt"
+mv "$REPONAME.tar.gz" "$TEMP_DIR"
+export ARTIFACT_ZIP_PATH="$TEMP_DIR/$REPONAME.tar.gz"
 tar -tf "$ARTIFACT_ZIP_PATH"
 
+echo "TEMP_DIR: $TEMP_DIR"
 echo "ARTIFACT_ZIP_PATH: $ARTIFACT_ZIP_PATH"
+echo "WORKINGDIR: $WORKINGDIR"
+
+exit 1
 
 
 
