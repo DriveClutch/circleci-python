5c95b6607c3a7e56e3d1cf2cd86f577c4d9a3361
Diana Caisin <44005801+dianacaisin@users.noreply.github.com>
2021-10-05 15:33:35 -0400

    Update archive.sh

--- tools/archive.sh
+++ tools/archive.sh
@@ -15,11 +15,7 @@ tar czf "${CIRCLE_PROJECT_REPONAME}".tar.gz -T "${CIRCLE_PROJECT_REPONAME}".txt
 rm -f "${CIRCLE_PROJECT_REPONAME}".txt
 
 mv "${CIRCLE_PROJECT_REPONAME}".tar.gz /tmp/archives
+
 #
 #export ARTIFACT="${CIRCLE_PROJECT_REPONAME}.tar.gz"
 #export ARTIFACT_ZIP_PATH="/tmp/archives/${CIRCLE_PROJECT_REPONAME}.tar.gz"
-
-
-
-
-
