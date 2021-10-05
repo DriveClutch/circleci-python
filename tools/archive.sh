d244376be3c4dd3dd005d6e5b7b1ffca6b2a547e
Diana Caisin <diana.caisin@coxautoinc.com>
2021-10-05 00:16:37 -0400

    feature/dc-cplat-271: archive.sh update

--- tools/archive.sh
+++ tools/archive.sh
@@ -8,18 +8,18 @@ mkdir -p /tmp/archives
 
 cd "${CIRCLE_WORKING_DIRECTORY}" || echo "no circle_working_directory defined"
 
-for filename in $(find . \( -name "*.py" -o -name "*.html" -o -name "*.htm" -o -name "Pipfile.lock" )\); do
-	print "${filename}" > "${CIRCLE_PROJECT_REPONAME}.txt"
-done
+filename="$(find . \( -name "*.py" -o -name "*.html" -o -name "*.htm" -o -name "Pipfile.lock" \))"
 
-tar czf "${CIRCLE_PROJECT_REPONAME}.tar.gz" -T "${CIRCLE_PROJECT_REPONAME}.txt"
+print "${filename}" > "${CIRCLE_PROJECT_REPONAME}".txt
 
-rm -f "${CIRCLE_PROJECT_REPONAME}.txt"
+tar czf "${CIRCLE_PROJECT_REPONAME}".tar.gz -T "${CIRCLE_PROJECT_REPONAME}".txt
 
-mv "${CIRCLE_PROJECT_REPONAME}.tar.gz" /tmp/archives
+rm -f "${CIRCLE_PROJECT_REPONAME}".txt
 
-export ARTIFACT="${CIRCLE_PROJECT_REPONAME}.tar.gz"
-export ARTIFACT_ZIP_PATH="/tmp/archives/${CIRCLE_PROJECT_REPONAME}.tar.gz"
+mv "${CIRCLE_PROJECT_REPONAME}".tar.gz /tmp/archives
+#
+#export ARTIFACT="${CIRCLE_PROJECT_REPONAME}.tar.gz"
+#export ARTIFACT_ZIP_PATH="/tmp/archives/${CIRCLE_PROJECT_REPONAME}.tar.gz"
 
 
 
